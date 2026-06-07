23f78dcf229e987732093f7b5dc801e2116e61ba
Evgeniy Titov <titevg@gmail.com>
2026-06-07 23:35:50 +0300

    add file with statuses

---
+++ statuses.py
@@ -0,0 +1,13 @@
+""" Статусы Пользователя в VK Bot """
+
+START_MESSAGING = 'start_messaging'
+VIEW_HELP = 'view_help'
+IN_MAIN_MENU = 'in_main_menu'
+VIEWING_FAVORITES = 'listing_favorites'
+CHOOSING_CITY = 'choosing_city'
+CHOOSING_GENDER = 'choosing_gender'
+CHOOSING_AGE_FROM = 'choosing_age_from'
+CHOOSING_AGE_TO = 'choosing_age_to'
+VIEWING_QUESTIONARES = 'viewing_questionnaires'
+ADD_TO_FAVORITE = 'add_to_favorite'
+VIEWING_FAVORITE_QUESTIONNAIRE = 'viewing_favorite_questionnaire'
