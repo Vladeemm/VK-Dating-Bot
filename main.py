c24497df7f8c8360b0e3a93bf3b4eafa969e95aa
Evgeniy Titov <titevg@gmail.com>
2026-06-19 21:31:14 +0300

    add logging

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-﻿from config import validate_config
+﻿import logging
+from config import validate_config
 from bot.core.bot import run_bot
 
-
 def main() -> None:
+    logging.basicConfig(level=logging.WARNING, filename='bot.log', filemode='a', format="%(name)s %(asctime)s %(levelname)s %(message)s")
+    
     validate_config()
     run_bot()
-
 
 if __name__ == '__main__':
     main()
