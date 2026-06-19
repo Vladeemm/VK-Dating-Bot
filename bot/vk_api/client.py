0619d5bccc5fec3ff50055e4c3b028ee791cb782
Evgeniy Titov <titevg@gmail.com>
2026-06-19 23:30:05 +0300

    add type hinting into client.py

--- bot/vk_api/client.py
+++ bot/vk_api/client.py
@@ -1,20 +1,21 @@
 """VK API клиент."""
 
 import os
+from typing import Any, Optional
 from dotenv import load_dotenv
 import vk_api
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-MY_VK_TOKEN = os.getenv('MY_VK_TOKEN')
+BOT_TOKEN: Optional[str] = os.getenv('BOT_TOKEN')
+MY_VK_TOKEN: Optional[str] = os.getenv('MY_VK_TOKEN')
 
-vk_group_session = vk_api.VkApi(token=BOT_TOKEN)
-vk_group_api = vk_group_session.get_api()
+vk_group_session: vk_api.VkApi = vk_api.VkApi(token=BOT_TOKEN)
+vk_group_api: Any = vk_group_session.get_api()
 
-vk_user_session = vk_api.VkApi(token=MY_VK_TOKEN)
-vk_user_api = vk_user_session.get_api()
+vk_user_session: vk_api.VkApi = vk_api.VkApi(token=MY_VK_TOKEN)
+vk_user_api: Any = vk_user_session.get_api()
 
 # Alias for legacy use in vk_api modules.
-vk = vk_user_api
-vk_group = vk_group_api
+vk: Any = vk_user_api
+vk_group: Any = vk_group_api
